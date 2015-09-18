3239c49a045e2cff1ac81363379def473c5480e1
Terry Marr <terrymarr280@gmail.com>
2015-09-18 09:40:28 -0700

    returns a new pie

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,32 @@
-var Topping = function(name, price) {
+var ItemPrice = new Object();
+  ItemPrice['small'] = 7;
+  ItemPrice['medium'] = 9;
+  ItemPrice['large'] = 12;
+  ItemPrice['pepperoni'] = 1;
+  ItemPrice['mushroom'] = .5;
+  ItemPrice['onion'] = .5;
+  ItemPrice['ham'] = 1;
+  ItemPrice['bacon'] = 1;
+  ItemPrice['pineapple']=.75;
+  ItemPrice['anchovies']=.5;
+  ItemPrice['olives']=.25;
+  ItemPrice['extra cheese']=.5;
+  ItemPrice['fresh basil']=.75;
+
+var Topping = function(name) {
   this.name = name;
-  this.price = price;
+  this.price = ItemPrice[name];
+};
+
+var Pizza = function(size) {
+  this.size = size;
+  this.basePrice = ItemPrice[size];
+  this.toppings = [];
+  this.cost = function() {
+    var cost = this.basePrice;
+    this.toppings.forEach(function(topping) {
+      cost += topping.price;
+    });
+    return cost;
+  };
 };
--- spec/specs.js
+++ spec/specs.js
@@ -1,12 +1,14 @@
-// describe('leapYear', function() {
-//   it("is false for a year that is not divisible by 4", function() {
-//     expect(leapYear(1999)).to.equal(false);
-//   });
-// });
 
 describe('topping', function() {
   it("returns the cost of a topping", function() {
-    var top = new Topping('pepperoni', 1);
+    var top = new Topping('pepperoni');
     expect(top.price).to.equal(1);
+  });
+});
+
+describe('pizza', function() {
+  it("returns a new pie", function() {
+    var pizza = new Pizza('small');
+    expect(pizza.size).to.equal('small');
   });
 });
